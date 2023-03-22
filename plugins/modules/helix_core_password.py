60123d4902744d5792c4959c60a592afc073f163
Luke James <luke-james@outlook.com>
2023-03-22 19:36:04 +0000

    Added option for different types of password resets

--- plugins/modules/helix_core_password.py
+++ plugins/modules/helix_core_password.py
@@ -156,9 +156,9 @@ def run_module():
     try:
         # modify password for a given user
         if module.params['oldpassword'] != '':
-            p4.run_password("-O", module.params['oldpassword'], "-P", module.params['newpassword'], module.params['name'])
+            p4.run("passwd", "-O", module.params['oldpassword'], "-P", module.params['newpassword'], module.params['name'])
         else:    
-            p4.run_password("-P", module.params['newpassword'], module.params['name'])
+            p4.run("passwd", "-P", module.params['newpassword'], module.params['name'])
         result['changed'] = True
 
     except Exception as e:
